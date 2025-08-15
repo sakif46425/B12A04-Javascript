07cee9f4bce977e7ed54e4ad21b982cd21ffcc7b
Sakif Khan <sakifkhan1340@gmail.com>
2025-08-15 17:15:41 +0600

    Finish the File of problem_3.js and aslo paste to final.js file

--- problem_3.js
+++ problem_3.js
@@ -1,41 +1,3 @@
-//Problem-01 : Train TT's Fine Calculator 
-
-function totalFine( fare ) {
-          if (typeof fare !== "number" || fare <= 0)
-          {
-            return "Invalid fare";
-          }
-
-          let fine = fare + fare * (20/100) + 30;
-          return fine;
-
-}
-
-
-
-
-
-
-// Problem-02 : Clean & Capitalize Characters
-
-function  onlyCharacter( str ) {
-          if (typeof str !== "string" )
-          {
-            return "Invalid";
-          }
-
-          let result = str.replace(/\s+/g, "").toUpperCase();
-
-          return result;
-}
-
-
-
-
-
-
-// Problem-03 : FIFA Best Team Award 
-
 function  bestTeam( player1, player2 ) {
           if (typeof player1 !== "object" || typeof player2 !== "object" || player1 === null || player2 === null) 
         {
@@ -61,14 +23,13 @@ function  bestTeam( player1, player2 ) {
         }
 }
 
+// console.log(bestTeam({ name: "Brazil", foul: 5, cardY: 1, cardR: 0 },
+// { name: "Argentina", foul: 7, cardY: 0, cardR: 0 }));
 
+// console.log(bestTeam({ name: "Germany", foul: 12, cardY: 0, cardR: 0 },
+// { name: "Sweden", foul: 7, cardY: 4, cardR: 1 }));
 
+// console.log(bestTeam({ name: "Germany", foul: 10, cardY: 1, cardR: 1 },
+// { name: "France", foul: 10, cardY: 2, cardR: 1 }));
 
-
-
-
-
-
-
-
-
+// console.log(bestTeam({ name: "Germany", foul: 10, cardY: 1, cardR: 1 },"France"));
